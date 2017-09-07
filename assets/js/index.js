190a6b3ef81bcf5bbdf6105d50b666de331f9f0d
kmaguswira <km.aguswira@gmail.com>
2017-09-07 16:38:00 +0800

    add newProduct ui

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 const {remote} = require('electron');
-const app = angular.module('app', ['ngRoute']);
+const app = angular.module('app', ['ngRoute', 'angucomplete']);
+const db = require('diskdb').connect(__dirname+'/store', ['products','sales','settings']);
 
 app.config(($routeProvider)=>{
   $routeProvider
@@ -39,28 +40,48 @@ app.controller('dashboardController', function($scope){
 });
 
 app.controller('posController', function($scope){
+  $scope.inventories = [
+    {id:"123", name:"name1", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name2", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name3", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name4", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name5", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name6", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+    {id:"123", name:"name7", stock:"30", base_price:"20000", sell_price:30000, updatedAt:"10000"},
+  ];
+  $scope.cart = [];
+  $scope.totalPrice = 0;
+  $scope.inventories.forEach((x,i)=>{$scope.cart[i]={active:false, qt:0, price:0};});
   $scope.testing = () => {
     alert("pos");
   };
   $scope.addToCart = (i) => {
-    console.log($scope.inventories[i]);
+    $scope.cart[i].active=true;
+    $scope.cart[i].price=$scope.inventories[i].sell_price;
+    $scope.cart[i].qt=1;
+    $scope.updateTotal();
   };
-  $scope.inventories = [
-    {id:"123", name:"name1", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name2", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name3", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name4", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name5", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name6", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-    {id:"123", name:"name7", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
-  ];
+  $scope.removeItem = (i) => {
+    $scope.cart[i].active=false;
+    $scope.cart[i].price=0;
+    $scope.cart[i].qt=0;
+    $scope.updateTotal();
+  };
+  $scope.updatePrice = (i,name) =>{
+    $scope.cart[i].qt=angular.element('#'+name).val();
+    $scope.cart[i].price=$scope.cart[i].qt*$scope.inventories[i].sell_price;
+    $scope.updateTotal();
+  };
+  $scope.getValForm = (i) => {
+    return angular.element('#'+i).val();
+  };
+  $scope.updateTotal = () => {
+    $scope.totalPrice = $scope.cart.reduce((total, x)=>{return total+x.price;},0);
+  };
 });
 
 app.controller('inventoryController', function($scope){
-  $scope.testing = () => {
-    alert("inventory");
-  };
-  $scope.inventories = [
+  $scope.products = [
     {id:"123", name:"name1", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
     {id:"123", name:"name2", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
     {id:"123", name:"name3", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
@@ -69,6 +90,9 @@ app.controller('inventoryController', function($scope){
     {id:"123", name:"name6", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
     {id:"123", name:"name7", stock:"30", base_price:"20000", sell_price:"30000", updatedAt:"10000"},
   ];
+  $scope.addNewProduct = () => {
+    console.log('test')
+  };
 });
 
 app.controller('configController', function($scope){
